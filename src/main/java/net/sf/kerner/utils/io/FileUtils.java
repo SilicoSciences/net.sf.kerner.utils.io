dbdc190c31f33368f50651bd81398d00222fbb71
Kerner <alex.kerner.24@googlemail.com>
2011-07-06 12:32:55 +0200

    [Feature] Introduce FileUtils.

---
+++ src/main/java/net/sf/kerner/utils/io/FileUtils.java
@@ -0,0 +1,37 @@
+package net.sf.kerner.utils.io;
+
+import java.io.File;
+
+public class FileUtils {
+	
+	private FileUtils(){}
+	
+	/**
+	 * Extended accessibility test, if a directory is available for reading. <br>
+	 * it consists of following tests:
+	 * <p>
+	 * {@code dir.exists() && dir.canRead() && dir.isDirectory() && dir.length()
+	 * != 0}
+	 * <p>
+	 * 
+	 * @param dir
+	 *            directory, that is checked.
+	 * @param createIfAbsend
+	 *            if {@code !dir.exists()}, it will be created.
+	 * @return true, if dir is accessible, false otherwise.
+	 */
+	public static boolean dirCheck(File dir, boolean createIfAbsend) {
+		if (createIfAbsend) {
+			if (dir.exists())
+				return (dir.canRead() && dir.isDirectory() && dir.length() != 0);
+			else {
+				final boolean b = dir.mkdirs();
+				return b;
+			}
+		} else {
+			return (dir.exists() && dir.canRead() && dir.isDirectory() && dir
+					.length() != 0);
+		}
+	}
+
+}
