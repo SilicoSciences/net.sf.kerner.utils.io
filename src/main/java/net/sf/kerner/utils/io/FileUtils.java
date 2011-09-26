2f430b70f11bacf3a15a4dd2bf9f0b4bbd99790a
Kerner <alex.kerner.24@googlemail.com>
2011-09-26 10:19:31 +0200

    [Feature] Introduce FileUtils#dirCheck(), #delete().

--- src/main/java/net/sf/kerner/utils/io/FileUtils.java
+++ src/main/java/net/sf/kerner/utils/io/FileUtils.java
@@ -1,6 +1,7 @@
 package net.sf.kerner.utils.io;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileUtils {
 	
@@ -31,6 +32,71 @@ public class FileUtils {
 			return (dir.exists() && dir.canRead() && dir.isDirectory() && dir
 					.length() != 0);
 		}
+	}
+	
+	/**
+	 * <p>
+	 * Extended accessibility test, if a file is available for reading. <br>
+	 * it consists of following tests:
+	 * <ul>
+	 * <li>
+	 * {@code file.exists()}</li>
+	 * <li>
+	 * {@code file.canRead()}</li>
+	 * <li>
+	 * {@code file.isFile()}</li>
+	 * </p>
+	 * 
+	 * @param file
+	 *            file, that is checked.
+	 * @param createIfAbsend
+	 *            if {@code !file.exists()}, it will be created.
+	 * @return true, if file is accessible; false otherwise.
+	 */
+	public static boolean fileCheck(File file, boolean createIfAbsend) {
+		if (createIfAbsend) {
+			synchronized (file) {
+				if (file.exists())
+					return (file.canRead() && file.isFile());
+				else {
+					try {
+						return file.createNewFile();
+					} catch (Exception e) {
+						return false;
+					}
+				}
+			}
+		} else {
+			return (file.exists() && file.canRead() && file.isFile());
+		}
+	}
+	
+	public static boolean delete(File file) throws IOException {
+		final boolean dirCheck = dirCheck(file, false);
+		final boolean fileCheck = fileCheck(file, false);
+		
+		if(!dirCheck && !fileCheck)
+			throw new IOException("cannot access [" + file + "]");
+		
+		if(dirCheck)
+			return deleteDir(file);
+		else
+			return file.delete();
+	}
+	
+	public static boolean deleteDir(File dir) throws IOException {
+		final boolean dirCheck = dirCheck(dir, false);
+		
+		if(!dirCheck)
+			throw new IOException("cannot access [" + dir + "]");
+			
+		final File[] files = dir.listFiles();
+		for(File f : files){
+			if(!delete(f))
+				return false;
+		}
+		
+		return true;
 	}
 
 }
